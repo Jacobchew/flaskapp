8ed81967949bf2bc9a7eaecbaf8b9b79121fcfeb
Jacobchew <jc_lifesuc@hotmail.com>
2013-04-01 00:08:48 +0100

    sessions and previous mistakes

--- app2.py
+++ app2.py
@@ -11,7 +11,7 @@ def dynamicroutingbiscuit(post_id):
 	return render_template('dyroutingbiscuit.html', post_id = post_id)
 
 # HTTP methods
-@app.route('/biscuit'/'HTTPmethods', methods=['GET', 'POST'])
+@app.route('/biscuit/HTTPmethods', methods=['GET', 'POST'])
 def HTTPmethodsbiscuit():
 	if request.method == 'POST':
 		Percentagedone = 30
@@ -21,7 +21,7 @@ def HTTPmethodsbiscuit():
 		return render_template('HTTPmethods.html', Percentagedone = Percentagedone)
 	
 # request data
-@app.route('/biscuit'/'login', methods=['GET', 'POST'])
+@app.route('/biscuit/login', methods=['GET', 'POST'])
 def requestdatalogin():
 	if request.data == 'POST':
 		username = request.form['username']
@@ -29,4 +29,19 @@ def requestdatalogin():
 		age = request.form['age']
 		return render_template('requestdatalogin.html, **locals())
 	return render_template('requestdatalogin.html, error = error)
+
+# Sessions, flash
+app.secret_key = '/0Ne/5t3p/clO5Er/14'
+@app.route('biscuit/Sessions',	
+def storeSessions():
+	session['age'] = 21
+	flash('age is stored in this session')
+	return redirect(url_for('index')
+def checkSession():
+	checkSession = session['age']
+	flash('age is still stored in this session')
+	return render_template('checkSession.html', checkSession = checkSession)
+def takeoutSession():
+	session.pop('age', None)
+	return redirect(url_for('index'))
 	
